0882c32b7f5985c08776b24388bb584eb236c252
Thomas <thom.cherryhomes@gmail.com>
2022-12-11 12:51:39 -0600

    start work on nhandler for apple2.

---
+++ apple2/ndev/fntools.s
@@ -0,0 +1,378 @@
+;
+; BASIC.SYSTEM POC for FNTOOLS Commands
+;
+; Adds extended commands to BASIC.SYSTEM. 
+; The commands are found in a table near the end of this
+; program.
+;
+; Rather than having an binary file for each FN command
+; (each requiring a redunant command line  parsing routine),
+; this will test the command line for any commands found 
+; in the table and jump to its corresponding subroutine.
+;
+; Author: Michael Sternberg
+;  <mhsternberg@gmail.com
+;
+; Adapted from
+; "Apple ProDOS: Advanced Features for Programmers" (Little)
+;
+;
+            .setcpu     "6502"
+            .segment    "SEG1"
+
+            ; Macros to set high bit for Apple text
+            .include "apple2text.inc"
+
+SBLOCK      :=      $3C         ;Parameters for block move
+EBLOCK      :=      $3E
+FBLOCK      :=      $42
+HIMEM       :=      $73         ;Use this as ONLINE buffer
+                    
+IN          :=      $0200       ;Command input buffer
+                    
+EXTRNCMD    :=      $BE06       ;External command JMP instruction
+ERROUT      :=      $BE09       ;Error handler
+XTRNADDR    :=      $BE50       ;Start of external cmd handler
+XLEN        :=      $BE52       ;External cmd name length (-1)
+XCNUM       :=      $BE53       ;Command # (0 for external)
+PBITS       :=      $BE54       ;Command parameter bits
+FBITS       :=      $BE56       ;Parameters fond in parse
+VPATH1	    :=      $BE6C	;pathname specified
+VSLOT       :=      $BE61       ;Slot parameter specified
+VDRIV       :=      $BE62       ;Drive parameter specified
+GETBUFR     :=      $BEF5       ;Get a free space
+                    
+MLI         :=      $BF00       ;Entry point to MLI
+                    
+CROUT       :=      $FD8E       ;Print a CR
+COUT        :=      $FDED       ;Std. character output subroutine
+MOVE        :=      $FE2C       ;Block move subroutine
+
+NET 	    :=      $07         ; Hardcoded network device for now.
+	
+            .org    $2000
+
+;-----------------------------------------------
+; Calculate # of pages that we need to reserve:
+;-----------------------------------------------
+            SEC
+            LDA     #>END
+            SBC     #>CMDCODE
+            STA     PAGES
+            INC     PAGES
+                    
+            LDA     PAGES
+            JSR     GETBUFR     ;Reserve the pages for the
+            BCC     INSTALL     ; command handler
+                    
+            LDA     #14         ;"PROGRAM TOO LARGE" error
+            JMP     ERROUT
+
+INSTALL:    STA     PGSTART     ;Save starting page #
+
+;---------------------------------------
+; Install the new command handler:
+;---------------------------------------
+            LDA     EXTRNCMD+1  ;Set up link to
+            STA     NEXTCMD+1   ; existing external command.
+            LDA     EXTRNCMD+2
+            STA     NEXTCMD+2
+
+;****************************************
+;* Install the external command handler *
+;* by storing its address after the     *
+;* JMP at EXTRNCMD.                     *
+;****************************************
+            LDA     #$00
+            STA     EXTRNCMD+1
+            LDA     PGSTART
+            STA     EXTRNCMD+2
+
+;---------------------------------------
+; Relocate the code
+;---------------------------------------
+            LDA     PGSTART     ;Get new page #
+            STA     reloc_00+2
+            STA     reloc_01+2
+            STA     reloc_02+2
+            ;STA     reloc_03+2
+            STA     reloc_04+2
+            STA     reloc_05+2
+            STA     reloc_06+2
+            STA     reloc_07+2
+            STA     reloc_08+2
+            STA     reloc_09+2
+            STA     reloc_10+2
+            STA     reloc_11+2
+            STA     reloc_12+2
+            STA     reloc_13+2
+            STA     reloc_14+2
+            STA     reloc_15
+            STA     reloc_16
+            STA     reloc_17
+
+    ;-----------------------------------
+    ; Set up parameters for 
+    ; block move to final location:
+    ;-----------------------------------
+            LDA     #<CMDCODE
+            STA     SBLOCK
+            LDA     #>CMDCODE
+            STA     SBLOCK+1
+                    
+            LDA     #<END
+            STA     EBLOCK
+            LDA     #>END
+            STA     EBLOCK+1
+                    
+            LDA     #0
+            STA     FBLOCK
+            LDA     PGSTART
+            STA     FBLOCK+1
+                
+    ;-----------------------------------
+    ; Perform the relocation
+    ;-----------------------------------
+            LDY     #0
+            JMP     MOVE        ;Move it!
+
+PAGES:      .res    1           ;Length of command handler
+PGSTART:    .res    1           ;Starting page of command handler
+            .align  $0100       ; Align to next page boundary
+CMDCODE     :=      *
+
+;************************************
+; This is the command checker. It  *
+; scans the input buffer to see    *
+; if any of the commands in the command table
+; have been entered.
+;************************************
+
+            CLD                 ; Used as flag
+    ;------------------------------------
+    ; Skip leading spaces
+    ;------------------------------------
+            LDY     #$00        ; Init index to command table
+            LDX     #$FF        ; Init index to cmd line
+SKPSPC:     INX
+            LDA     IN,X        ; Get char from cmd line
+            CMP     #$A0        ; If space, go to next
+            BEQ     SKPSPC
+
+    ;------------------------------------
+    ; Get char from cmd line
+    ;------------------------------------
+            DEX                 ; Kludge
+CHKCMD:     INX                 ; Advance index to cmd line char
+CHKCMD1:    LDA     IN,X        ; Get cmd line char
+            CMP     #$8D        ; Terminate if EOL
+            BEQ     CMDSKP1     ; 
+            CMP     #$A0        ; Terminate if space
+            BNE     CMDSKP2     ; Otherwise use face value
+CMDSKP1:    LDA     #$00        ; Convert EOL or space to null
+
+    ;--------------------------------------------
+    ; Compare cmd line char to command table char
+    ;--------------------------------------------
+CMDSKP2:
+reloc_00:   CMP     COMMAND,Y   ; Do chars match?
+            BNE     SKPCMD      ; No. Skip to next cmd entry and try again
+
+    ;------------------------------------
+    ; Found matching character
+    ;------------------------------------
+reloc_01:   LDA     COMMAND,Y       ; Check if curr char is null ($00)
+            BEQ     SETRULES        ; At end of command string (null)?
+            CPY     #COMMAND_SIZE-1 ; At end of command table?
+            BEQ     NOTFOUND        ; If yes, quit
+            INY                     ; Advance to next char in command table
+            BNE     CHKCMD          ; Continue checking chars
+            
+    ;------------------------------------
+    ; Skip to next entry in command table
+    ;------------------------------------
+SKPCMD:     INY
+reloc_02:   LDA     COMMAND,Y       ; Skip to null char
+            BNE     SKPCMD
+            ;----
+            INY                     ; Advance to next command table entry
+            CPY     #COMMAND_SIZE   ; At end of command table?
+            BCS     NOTFOUND        ; Yes (Y>=COMMAND_SIZE), then quit
+            INY                     ; 
+            LDX     #$00            ; Reset index to cmd line
+            BEQ     CHKCMD1         ; Try next entry in command table
+
+;---------------------------------------
+NOTFOUND:   
+;---------------------------------------
+            SEC 
+NEXTCMD:    JMP     $0000           ;(Fill in when installed)
+
+;---------------------------------------
+SETRULES:
+;---------------------------------------
+            INY                     ; Advance to command index in table
+reloc_04:   LDA     COMMAND,Y       ; Get command index
+reloc_05:   STA     CMD             ; Store Command index
+            DEX
+            STX     XLEN            ; Store command length - 1
+            TAX
+reloc_06:   LDA     CMD_TAB_L,X     ; Get address of command subroutine
+            STA     XTRNADDR
+reloc_07:   LDA     CMD_TAB_H,X     ; Get address of command subroutine
+            STA     XTRNADDR+1
+
+            LDA     #$00
+            STA     XCNUM           ;External cmd number = 0
+
+;---------------------------------------
+; Set up string parsing rules:
+;---------------------------------------
+reloc_08:   LDA     PBITS_TAB_L,X
+            STA     PBITS
+reloc_09:   LDA     PBITS_TAB_H,X
+            STA     PBITS+1
+
+            LDA     HIMEM
+reloc_10:   STA     BUFFER
+            LDA     HIMEM+1
+reloc_11:   STA     BUFFER+1
+
+;---------------------------------------
+; Exit
+;---------------------------------------
+            CLC                     ;Clear carry to indicate success
+            RTS                     ;Finished
+
+;---------------------------------------
+; BASIC.SYSTEM comes here after it has
+; successfully parsed the command line:
+;---------------------------------------
+
+;---------------------------------------
+DO_NCD:     
+;---------------------------------------
+NCD1: 
+	LDX	#$00
+	LDA	#$00
+:	STA	$0200,X
+	INX
+	BNE	:-
+	LDA	FBITS
+	AND	#$01		; Check for filename
+	BNE	NCD2
+	LDA	#$01		; zero out len
+	STA	$0200
+	BCC	NCD35		; Send out empty payload to reset ncd
+	;;
+NCD2:	LDA	VPATH1
+	STA	$FA
+	LDA	VPATH1+1
+	STA	$FB
+	LDY	#$00		; Offset 0 - String length
+	STY	$0201
+	LDA	($FA),Y		; Get length
+	TAX
+	STA	$FC		; Store it.
+	INX			;
+	STX	$0200		; Store it in length low.
+	LDA	FBITS
+NCD3:   INY				
+	LDA	($FA),Y		; Next char
+	STA	$0201,Y		; Copy it
+	CPY	$FC		; At end?
+	BNE	NCD3		; Nope, continue.
+NCD35:	JSR	$C50D
+	.BYTE	$04
+	.WORD	NCDOP
+	
+	BCC	NCD4
+	LDA	#$02
+NCD4:	RTS
+
+reloc_12:	
+NCDOP:	
+	.byte	$03		; Control has 3 params
+	.byte	NET		; to Network device
+	.word	IN		; Buffer is at $0200
+	.byte	','		; NCD command
+
+NCD_STR:    ASCIIHIZ    "Executing NCD"
+
+;---------------------------------------
+DO_NPWD:     
+;---------------------------------------
+            LDX     #$00
+NPWD_LOOP:  
+reloc_13:   LDA     NPWD_STR,X
+            BEQ     NPWD_DONE
+            JSR     COUT
+            INX
+            BNE     NPWD_LOOP
+NPWD_DONE:  RTS
+
+NPWD_STR:   ASCIIHIZ    "Executing NPWD"
+
+;---------------------------------------
+DO_NTRANS:     
+;---------------------------------------
+            LDX     #$00
+NTRANS_LOOP:  
+reloc_14:   LDA     NTRANS_STR,X
+            BEQ     NTRANS_DONE
+            JSR     COUT
+            INX
+            BNE     NTRANS_LOOP
+NTRANS_DONE: 
+            RTS
+
+NTRANS_STR: ASCIIHIZ    "Executing NTRANS"
+
+CMD:        .res    1           ; Index to matched command (0=NCD, 1=NPWD, ..)
+
+;---------------------------------------
+; Command Tables
+;---------------------------------------
+            .enum CMD_IDX
+                NCD
+                NPWD
+                NTRANS
+            .endenum
+
+COMMAND:    ASCIIHI "NCD"
+            .byte   $00
+            .byte   CMD_IDX::NCD
+
+            ASCIIHI "NPWD"
+            .byte   $00
+            .byte   CMD_IDX::NPWD
+
+            ASCIIHI "NTRANS"
+            .byte   $00
+            .byte   CMD_IDX::NTRANS
+
+COMMAND_SIZE = * - COMMAND - 1
+
+CMD_TAB_L:  
+            .byte   <DO_NCD
+            .byte   <DO_NPWD
+            .byte   <DO_NTRANS
+
+CMD_TAB_H:  
+reloc_15:   .byte   >DO_NCD
+reloc_16:   .byte   >DO_NPWD
+reloc_17:   .byte   >DO_NTRANS
+
+PBITS_TAB_L:
+            .byte   $10         ; 0 - NCD
+            .byte   $10         ; 1 - NPWD
+            .byte   $10         ; 2 - NTRANS
+
+PBITS_TAB_H:  
+            .byte   $04         ; 0 - NCD
+            .byte   $04         ; 1 - NPWD
+            .byte   $04         ; 2 - NTRANS
+
+BUFFER:     .addr   $0000
+
+END         :=      *
